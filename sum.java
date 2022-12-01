ad5497c3ac88bfed6e258237855c461fd42d7c39
Raj kataria <katariaraj2022@gmail.com>
2022-12-01 17:30:26 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,9 +2,10 @@ public class sum
 {
     public static void main(string args[])
     {    
-int a=1,b=5;
-sum=a+b;
-System.out.println(sum);
+int a=1;
+int b=3;
+
+System.out.println("the sum is +"a+b);
     }
 }
 
